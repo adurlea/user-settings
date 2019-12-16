0dbd37aff45b5b6008b1af235a2962dcf90b9d6d
Adrian DURLEA <adrian.durlea@gmail.com>
2019-12-16 11:39:27 +0100

    Chapter 5 Implementations: - onSubit form validations - form controle events

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IUserSettings } from '../data/user-settings';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -22,4 +23,11 @@ export class UserSettingsFormComponent implements OnInit {
   ngOnInit() {
   }
 
+  onBlur(field: NgModel) {
+    console.log('In onBlur: ' + field.valid)
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('In onSubmit: ' + form.valid);
+  }
 }
