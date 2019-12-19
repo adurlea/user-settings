2f920a2eb0163b4cf9df8cd40b95da0e7f45bf7b
Adrian DURLEA <adrian.durlea@gmail.com>
2019-12-19 21:30:17 +0100

    Chapter 6 Implementations: - http client - post a form

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { IUserSettings } from './user-settings';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
-  postUserSettingsForm(usserSettings: IUserSettings): Observable<IUserSettings> {
-    return of(usserSettings);
+  postUserSettingsForm(usserSettings: IUserSettings): Observable<any> {
+    return this.httpClient.post('https://putsreq.com/YQQykqWn4a28jo1Ag3uj', usserSettings);
+    // return of(usserSettings);
   }
 }
